5ee3ebeb9432fe20d86a570aa65406bf8759bd23
evan <evanmarke@gmail.com>
2020-08-02 12:27:43 -0400

    new length_ascii approach

--- src/password_validator.py
+++ src/password_validator.py
@@ -4,49 +4,36 @@ import csv
 import argparse as argp
 
 
+# Settings for Password Valid Length
+# Default to [8,64] if not otherwise specified
+MIN_LENGTH = getattr(
+    settings, "MIN_LENGTH", 8)
+MAX_LENGTH = getattr(
+    settings, "MAX_LENGTH", 64)
+
 #TODO create class for OOP
-class LengthAsciiRequirement:
+class LengthAsciiRequirement(object):
     """For candidate password, checks if length = [8,64]
      and contains only ASCII characters.
      """
-    def __init__(self):
-        pass
+    def __init__(self, min_length=None, max_length=None):
+        self.min_length = min_length
+        self.max_length = max_length
 
-    # returns True if all characters are ASCII, false otherwise
-    # also checks for ascii control characters through printable function to preclude decoded ASCII control characters
-    def password_is_ascii(s):
-        # return len(s) == len(s.encode())
-        # regex only for earlier versions of python
-        # password_is_ascii = lambda s: re.match('^[\x00-\x7F]+$', s) != None
-        #TODO use [^\x20-\x7E] instead of above to remove ascii control characters 0-31,127. and replace
-        # non-ascii characters with asterixes 
-        # s_asterisk = re.sub('^[\x00-\x7F]', '*', s)
-        # fix try/except. syntax all off. change approach in morning :-|
-        while True: 
-            try:
-                s.isascii() and s.isprintable() = True
-                break
-            except: 
-                s_asterisk = re.sub('^[\x00-\x7F]', '*', s)
-                print(s_asterisk, '-> Error: Invalid Characters')
-
-    # returns True if input at least 8 characters
-    def too_short(s): 
-        while False: 
-            try:
-                len(s) < 8 == False
-                break
-            except: 
-                print(s, '-> Error: Too Short')
-
-    # returns True if input no more than 64 characters
-    def too_long(s): 
-        while False: 
-            try:
-                len(s) > 64 == False 
-                break
-            except: 
-                print(s, '-> Error: Too Long')
+    def __call__(self, value):
+            """returns True if all characters are ASCII, false otherwise.
+            Also checks for ascii control characters through printable 
+            function to preclude decoded ASCII control characters
+            """
+        if value.ascii() and value.isprintable() == False
+            censored_value = re.sub('^[\x00-\x7F]', '*', value) #replaces non-ASCII char with *
+            print(censored_value, '-> Error: Invalid Characters')
+            #if non-ASCII test fails, no need to check further
+            break 
+        elif self.min_length is not None and len(value) < self.min_length:
+            print(value, '-> Error: Too Short. ','Must be more than %s characters') % self.min_length
+        elif self.max_length is not None and len(value) > self.max_length:
+            print(value, '-> Error: Too Long. ','Must be less than %s characters') % self.max_length
 
 class WeakPassword:  
     def __init__(self, s, weak_password_list):
