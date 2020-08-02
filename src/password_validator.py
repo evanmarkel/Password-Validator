0e6935272d5f8442763f9d5d927421e3914d3471
evan <evanmarke@gmail.com>
2020-08-01 22:47:25 -0400

    Update password_validator.py

--- src/password_validator.py
+++ src/password_validator.py
@@ -1,18 +1,24 @@
 import sys
 import re
 import csv
+import argparse as argp
+
+
+# returns True if input at least 8 characters
+def too_short(s): 
+    return len(s) < 8
+
+# returns True if input no more than 64 characters
+def too_long(s): 
+    return len(s) > 64
 
 # returns True if all characters are ASCII, false otherwise
 # also checks for ascii control characters through printable function to preclude decoded ASCII control characters
 def password_is_ascii(s):
     # return len(s) == len(s.encode())
+    # regex only for earlier versions of python
+    # password_is_ascii = lambda s: re.match('^[\x00-\x7F]+$', s) != None
     return s.is_ascii() and s.isprintable()
 
-# returns True if input at least 8 characters
-def too_short(s): 
-    return len(s) > 7
-
-# returns True if input no more than 64 characters
-def too_long(s): 
-    return len(s) < 65
+Class WeakPassword
 
